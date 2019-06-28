48ca8751b495400fc033af7f1250b56b7d820f51
Roy <luogao_lg@sina.com>
2019-06-28 17:24:13 +0800

    [update] 有状态widget/无状态widget

--- startup_namer/lib/main.dart
+++ startup_namer/lib/main.dart
@@ -15,6 +15,53 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class FavoriteWidget extends StatefulWidget {
+  @override
+  _FavoriteWidgetState createState() => new _FavoriteWidgetState();
+}
+
+class _FavoriteWidgetState extends State<FavoriteWidget> {
+  bool _isFavorited = true;
+  int _favoriteCount = 41;
+
+  void _toggleFavorite() {
+    setState(() {
+      if (_isFavorited) {
+        _favoriteCount -= 1;
+        _isFavorited = false;
+      } else {
+        _favoriteCount += 1;
+        _isFavorited = true;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Row(
+      children: <Widget>[
+        new Container(
+          padding: new EdgeInsets.all(0),
+          child: new IconButton(
+            icon: (_isFavorited
+                ? new Icon(Icons.star)
+                : new Icon(Icons.star_border)),
+            color: Colors.red[500],
+            onPressed: _toggleFavorite,
+          ),
+        ),
+        new SizedBox(
+          width: 18.0,
+          child: new Container(
+            child: new Text('$_favoriteCount'),
+          ),
+        ),
+      ],
+      mainAxisSize: MainAxisSize.min,
+    );
+  }
+}
+
 class Layout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -54,11 +101,7 @@ Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese Alps. Situate
               ],
             ),
           ),
-          new Icon(
-            Icons.star,
-            color: Colors.red[500],
-          ),
-          new Text('41'),
+          new FavoriteWidget()
         ],
       ),
     );
@@ -102,6 +145,7 @@ Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese Alps. Situate
         titleSection,
         buttonSection,
         textSection,
+        
       ]),
     );
   }
